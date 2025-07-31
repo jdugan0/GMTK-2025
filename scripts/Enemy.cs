645dd882eaa8db811a79035804b5767779973bcd
jdugan0 <justin.d.dugan@gmail.com>
2025-07-30 21:48:24 -0400

    prototype done

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -11,12 +11,26 @@ public partial class Enemy : CharacterBody2D
     }
     public override void _PhysicsProcess(double delta)
     {
+        if (!IsInstanceValid(GameManager.instance.player)) return;
         nav.TargetPosition = GameManager.instance.player.GlobalPosition;
         if (!nav.IsTargetReached())
         {
             var dir = ToLocal(nav.GetNextPathPosition()).Normalized();
             Velocity = dir * maxSpeed;
             MoveAndSlide();
+        }
+        for (int i = 0; i < GetSlideCollisionCount(); i++)
+        {
+            var c = GetSlideCollision(i);
+            OnCol(c.GetCollider() as Node2D);
+        }
+    }
+
+    public void OnCol(Node2D node)
+    {
+        if (node is Movement)
+        {
+            node.QueueFree();
         }
     }
 

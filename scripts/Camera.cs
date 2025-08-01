2d51531164e8825af4fe346ccb1fb93d3120f2c3
jdugan0 <justin.d.dugan@gmail.com>
2025-08-01 10:46:26 -0400

    balance

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class Camera : Camera2D
 {
+    [Export] private float pulseAlpha = 0.35f;
+    [Export] private float pulseTime = 0.5f;
+    private ColorRect overlay;
+    private Tween tween;
+    private bool pulsing = false;
     public override void _Ready()
     {
         if (IsInstanceValid(GameManager.instance.player))
@@ -19,7 +24,26 @@ public partial class Camera : Camera2D
         if (IsInstanceValid(GameManager.instance.player))
         {
             GlobalPosition = GameManager.instance.player.GlobalPosition;
+
+            if (GameManager.instance.player.health == 1 && !pulsing)
+                StartPulse();
+            else if (GameManager.instance.player.health > 1 && pulsing)
+                StopPulse();
         }
+    }
+    private void StartPulse()
+    {
+        pulsing = true;
+        tween = GetTree().CreateTween().SetLoops();
+        tween.TweenProperty(overlay, "modulate:a", 0.0f, pulseTime).From(pulseAlpha);
+        tween.TweenProperty(overlay, "modulate:a", pulseAlpha, pulseTime);
+    }
+
+    private void StopPulse()
+    {
+        pulsing = false;
+        tween?.Kill();
+        overlay.Modulate = overlay.Modulate with { A = 0 }; 
     }
 
 }

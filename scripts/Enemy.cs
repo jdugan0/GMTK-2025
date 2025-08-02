10c8b289a87f90fd96fa799df5ca06a0fce038de
jdugan0 <justin.d.dugan@gmail.com>
2025-08-02 10:52:19 -0400

    enemy sounds

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -27,7 +27,8 @@ public partial class Enemy : CharacterBody2D
     {
         NORMAL,
         RANGED,
-        PROT
+        PROT,
+        FAST
     }
     [Export] EnemyTypes enemyType = EnemyTypes.NORMAL;
 
@@ -40,7 +41,7 @@ public partial class Enemy : CharacterBody2D
             health--;
             return;
         }
-        
+
         Node2D ghostNode = ghost.Instantiate<Node2D>();
         GetTree().CurrentScene.AddChild(ghostNode);
         ghostNode.GlobalPosition = GlobalPosition;
@@ -86,6 +87,15 @@ public partial class Enemy : CharacterBody2D
                 {
                     GameManager.instance.player.TakeDamage();
                 }
+                AudioManager.instance.PlaySFX("slash");
+                break;
+            case EnemyTypes.FAST:
+                sprite.Play("Attack");
+                if (playerIn)
+                {
+                    GameManager.instance.player.TakeDamage();
+                }
+                AudioManager.instance.PlaySFX("enemyFast");
                 break;
             case EnemyTypes.PROT:
                 sprite.Play("Attack");
@@ -93,6 +103,7 @@ public partial class Enemy : CharacterBody2D
                 {
                     GameManager.instance.player.TakeDamage();
                 }
+                AudioManager.instance.PlaySFX("enemyStrong");
                 break;
             case EnemyTypes.RANGED:
                 sprite.Play("Attack");

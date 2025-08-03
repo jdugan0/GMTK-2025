98099c16b20350986f58a2dae426ce2f3cf92cee
jdugan0 <justin.d.dugan@gmail.com>
2025-08-02 20:18:20 -0400

    intro tweaks

--- scripts/UI/GameEnd.cs
+++ scripts/UI/GameEnd.cs
@@ -5,14 +5,24 @@ using System.Threading.Tasks;
 public partial class GameEnd : Node
 {
     [Export] public float time = 5f;
+    [Export] public bool nextLevel = false;
+    [Export] public string nextScene = "GameComplete2";
     float timer = 0;
+
     public override async void _Process(double delta)
     {
         timer += (float)delta;
         if (timer > time)
         {
             timer = 0;
-            await SceneSwitcher.instance.SwitchSceneAsyncSlide("GameComplete2");
+            if (nextLevel)
+            {
+                GameManager.instance.NextLevel();
+            }
+            else
+            {
+                await SceneSwitcher.instance.SwitchSceneAsyncSlide(nextScene);
+            }
         }
     }
 

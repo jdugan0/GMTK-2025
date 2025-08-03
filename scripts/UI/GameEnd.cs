5af352e8599d9b1839f75d7309a031cc070d73b4
jdugan0 <justin.d.dugan@gmail.com>
2025-08-02 20:25:46 -0400

    ewihfewiugfhw

--- scripts/UI/GameEnd.cs
+++ scripts/UI/GameEnd.cs
@@ -8,9 +8,13 @@ public partial class GameEnd : Node
     [Export] public bool nextLevel = false;
     [Export] public string nextScene = "GameComplete2";
     float timer = 0;
-
+    
     public override async void _Process(double delta)
     {
+        if (Input.IsActionJustPressed("FIRE") && nextLevel)
+        {
+            GameManager.instance.NextLevel();
+        }
         timer += (float)delta;
         if (timer > time)
         {
